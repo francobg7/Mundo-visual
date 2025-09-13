a23bad65efb1aedcffd84cd696be1ae61223c314
francobg7 <francobg7@gmail.com>
2025-09-13 16:39:11 -0300

    modified

--- e-commerce/client/src/app/layout.tsx
+++ e-commerce/client/src/app/layout.tsx
@@ -33,8 +33,8 @@ export default function RootLayout({
         <div className="mx-auto p-4 sm:px-0 sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-6xl">
           <Navbar />
           {children}
-          <Footer />
         </div>
+        <Footer />
         <ToastContainer position="bottom-right" />
       </body>
     </html>
