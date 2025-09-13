89082297d0d59d9f4fa60db84a4d61a1b4bc4cf0
francobg7 <francobg7@gmail.com>
2025-09-13 16:50:56 -0300

    nav bar changed color

--- e-commerce/client/src/components/SearchBar.tsx
+++ e-commerce/client/src/components/SearchBar.tsx
@@ -2,9 +2,9 @@ import { Search } from "lucide-react"
 
 const SearchBar = () => {
   return (
-    <div className='hidden sm:flex items-center gap-2 rounded-md ring-1 ring-gray-200 px-2 py-1 shadow-md'>
-      <Search className="w-4 h-4 text-gray-500"/>
-      <input id="search" placeholder="Buscar..." className="text-sm outline-0"/>
+    <div className='hidden sm:flex items-center gap-2 rounded-md ring-1 ring-gray-600 px-2 py-1 shadow-md bg-gray-800'>
+      <Search className="w-4 h-4 text-gray-300"/>
+      <input id="search" placeholder="Buscar..." className="text-sm outline-0 bg-transparent text-white placeholder-gray-400"/>
     </div>
   )
 }
