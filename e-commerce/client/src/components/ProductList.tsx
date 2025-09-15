6d6c54f3f386f23a52af776f3b6fb572682524eb
francobg7 <francobg7@gmail.com>
2025-09-15 18:20:28 -0300

    grid modified

--- e-commerce/client/src/components/ProductList.tsx
+++ e-commerce/client/src/components/ProductList.tsx
@@ -247,7 +247,7 @@ const ProductList = ({ category,params }: { category: string, params:"homepage" 
           </p>
         </div>
       ) : (
-        <div className="grid grid-cols-1 xl:grid-cols-2 gap-12">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
           {filteredProducts.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
