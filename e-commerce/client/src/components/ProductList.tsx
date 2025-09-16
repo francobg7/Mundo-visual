b918d63937ba12c936cbff2692a0549eefbdc9dd
francobg7 <francobg7@gmail.com>
2025-09-16 14:20:06 -0300

    product card clean

--- e-commerce/client/src/components/ProductList.tsx
+++ e-commerce/client/src/components/ProductList.tsx
@@ -249,7 +249,7 @@ const ProductList = ({ category,params }: { category: string, params:"homepage" 
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
           {filteredProducts.map((product) => (
-            <ProductCard key={product.id} product={product} />
+            <ProductCard key={product.id} product={product} isHomepage={params === "homepage"} />
           ))}
         </div>
       )}
