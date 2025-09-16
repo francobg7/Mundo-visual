b88a9ba53adc3a039dc1fae258b4f0c0093251c9
francobg7 <francobg7@gmail.com>
2025-09-16 15:30:34 -0300

    hero deleted

--- e-commerce/client/src/app/layout.tsx
+++ e-commerce/client/src/app/layout.tsx
@@ -31,7 +31,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        {children}
+        <div className="pt-16">
+          {children}
+        </div>
         <Footer />
         <ToastContainer position="bottom-right" />
       </body>
